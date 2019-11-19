4b129a30784c88459651b81c0239ccf576bbacce
iamsagar7 <sagar77ghimire@gmail.com>
2019-11-19 16:25:02 +0545

    Updates

---
+++ precache-manifest.0487f2037934623048706171842ac1a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf686b0c35d548e36990ade110bc345",
+    "url": "/sagarghimire.github.io/index.html"
+  },
+  {
+    "revision": "e2ccfd753f2e4dba1881",
+    "url": "/sagarghimire.github.io/static/css/main.d7394254.chunk.css"
+  },
+  {
+    "revision": "3c7aca97115e27498f7b",
+    "url": "/sagarghimire.github.io/static/js/2.23f7a46b.chunk.js"
+  },
+  {
+    "revision": "e2ccfd753f2e4dba1881",
+    "url": "/sagarghimire.github.io/static/js/main.442fa1f6.chunk.js"
+  },
+  {
+    "revision": "9f834765464ee762d31d",
+    "url": "/sagarghimire.github.io/static/js/runtime~main.436700bd.js"
+  }
+]);
